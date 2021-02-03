308825dc224f404e88bf3f2d4bd162d6413f23db
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-02-03 09:53:23 +0600

    Double-VS-Tripple-Equal

---
+++ double-tripple-equal.js
@@ -0,0 +1,30 @@
+// --Double VS Tripple--//
+
+// ---Comparision Operator ---//
+1.Double Equal(==)--both side values are same (kina ta check korbe).
+2.Tripple Equal(===)--both side dataType and Value are same (kina ta check korbe).
+
+
+
+N.B--
+1.JS a --true k compare kore 1 er sthe (means --true = 1 dore nei initialy)
+2.JS a --false k compare kore 0 er sthe (means --false = 0 dore nei initialy).
+
+const first = 1;
+const second = true;
+if(first == second){
+    console.log("Conditions is true");
+}
+else{
+    console.log("Conditions is false");
+}
+
+const first = 0;
+const second = false;
+if(first == second){
+    console.log("Conditions is true");
+}
+else{
+    console.log("Conditions is false");
+}
+

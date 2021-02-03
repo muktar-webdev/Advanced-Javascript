8036c11b04375a53e65c874bea58377789d1a9e9
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-02-03 09:37:49 +0600

    undefined VS null

---
+++ null-VS-undefined.js
@@ -0,0 +1,30 @@
+// null--null means empty or non-existing value.Null is assigned, and explicitly means nothing
+
+// undefined--undefined means a variable has been declared but not defined yet.(not set or assign variable value yet)
+
+
+// ---how we get 'undefined'---//
+//1. let names ;
+
+// 2.const names = undefined;
+
+// 3.let numbers = (x,y) =>{
+//     console.log(x+y);
+    
+// }
+// const result = numbers(10,10)
+
+//4. let numbers = (x,y) => {
+//     console.log(x+y);
+//     return;
+// }
+// const result = numbers(20,40);
+// console.log(result);
+
+// 5.const person ={names:'nishat',phone:'0160109199'};
+// console.log(person.gf);
+
+
+// ---how we get null---//
+
+// null --not existing value. tar mane okhane kicu nei re vaap

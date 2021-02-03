c4c1f1c5b83d2ecef612fbdc0220f712bf045722
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-02-03 13:16:19 +0600

    splits

---
+++ splits.js
@@ -0,0 +1,17 @@
+// --slice--//
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+const part = numbers.slice(3,8);
+// console.log(part);
+// console.log(numbers);
+
+// --splice--//
+const count = [1,2,3,4,5,6,7,8,9,10];
+const part2 = numbers.splice(3,8);
+// console.log(part2);
+// console.log(count);
+
+
+// --join--//
+
+const join = numbers.join(',');
+console.log(join);
